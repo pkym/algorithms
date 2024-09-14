3ac830306c4bc2370f4095a3212616374df5b144
ymkee <pucky0531@gmail.com>
2024-09-14 20:19:32 +0900

    arrays

--- 01_arrays/4.mergeSortedArrays.js
+++ 01_arrays/4.mergeSortedArrays.js
@@ -5,14 +5,15 @@ function mergeSortedArrays(array1, array2){
     let i = 1;
     let j = 1;
     
-    //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
+    //Check input > 배열이 비었을 때 나머지 배열 반환
     if(array1.length === 0) {
       return array2;
     }
     if(array2.length === 0) {
       return array1;
-    }
-  
+    }  
+
+    // 조건 충족 하지 않기전까지 반복
     while (array1Item || array2Item){
      if(array2Item === undefined || array1Item < array2Item){
        mergedArray.push(array1Item);
@@ -28,4 +29,5 @@ function mergeSortedArrays(array1, array2){
     return mergedArray;
   }
   
-  mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+  console.log(mergeSortedArrays([0,3,4,31], [3,4,6,30]));
+  console.log([0,3,4,31].concat([3,4,6,30]).sort((a,b)=>(a-b)));
